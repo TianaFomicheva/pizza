9d04affe3e631a7750f031e657341a360a4ab7df
tianaFomicheva <tianirika@gmail.com>
2021-02-01 22:36:29 +0500

    totalPrice

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,7 +6,7 @@ const initialState = {
 const cart = (state = initialState, action)=>{
     switch (action.type) {
         case 'ADD_TO_CART':
-
+        const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
         const currentItem = 
             !state.items[action.payload.uniq] ? [action.payload] : [...state.items[action.payload.uniq].items, action.payload]
         
@@ -14,14 +14,19 @@ const cart = (state = initialState, action)=>{
             ...state.items,
             [action.payload.uniq]: {
                 items: currentItem,
+                totalPrice: getTotalPrice(currentItem)
 
               },
         }
         console.log(actualItems)    
-            return{
+        const res = {
                 ...state,
-                items: actualItems
+                items: actualItems,
+                totalPrice: Object.values(actualItems).reduce((sum, obj) => obj.totalPrice + sum, 0)
+
             }
+        console.log(res)
+            return res
     
         default:
             return state
